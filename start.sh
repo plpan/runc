50759a5a29d50f9e47f878921f78ef6547efe7ca
plpan <pplonepiece@126.com>
2019-11-21 19:02:13 +0800

    add build script

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sh test.sh b
+make -j 4
+sh test.sh f
---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cmd=$1
+
+if [ "$cmd" == "f" ]; then
+	if [ -f /bin/docker-runc.bak ]; then
+		exit -1
+	fi
+	mv /bin/docker-runc /bin/docker-runc.bak
+	cp /home/odin/panpeilong/go/src/github.com/opencontainers/runc/runc /bin/docker-runc
+elif [ "$cmd" == "b" ]; then
+	if ! [ -f /bin/docker-runc.bak ]; then
+		exit -1
+	fi
+	mv /bin/docker-runc.bak /bin/docker-runc
+fi
